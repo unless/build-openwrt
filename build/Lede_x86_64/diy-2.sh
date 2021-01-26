97e60ff8417ab5a14cbbbb516badce0eedf8ad7d
unless <jbpv@163.com>
2021-01-26 22:32:37 +0800

    Update diy-2.sh

--- build/Lede_x86_64/diy-2.sh
+++ build/Lede_x86_64/diy-2.sh
@@ -22,6 +22,13 @@ sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz
 #sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.19/g' target/linux/x86/Makefile
 #sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.19/g' target/linux/x86/Makefile
 
+echo "CONFIG_BINFMT_ELF=y" >>  target/linux/x86/config-5.4
+echo "CONFIG_ELFCORE=y" >>  target/linux/x86/config-5.4
+echo "# CONFIG_CORE_DUMP_DEFAULT_ELF_HEADERS is not set" >>  target/linux/x86/config-5.4
+echo "CONFIG_BINFMT_SCRIPT=y" >>  target/linux/x86/config-5.4
+echo "CONFIG_BINFMT_MISC=y" >>  target/linux/x86/config-5.4
+echo "CONFIG_COREDUMP=y" >>  target/linux/x86/config-5.4
+
 # 修改插件名字（修改名字后不知道会不会对插件功能有影响，自己多测试）
 #sed -i 's/"BaiduPCS Web"/"百度网盘"/g' package/lean/luci-app-baidupcs-web/luasrc/controller/baidupcs-web.lua
 #sed -i 's/cbi("qbittorrent"),_("qBittorrent")/cbi("qbittorrent"),_("BT下载")/g' package/lean/luci-app-qbittorrent/luasrc/controller/qbittorrent.lua
